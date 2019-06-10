938b70acc0e166fe8c82023edeaac3631626a2ce
vpascal <vpascal2@gmail.com>
2019-06-10 13:55:56 -0400

    byrace

---
+++ byrace.js
@@ -0,0 +1,43 @@
+var byrace = d3plus.viz().container("#byrace");
+d3.csv("data/byrace.csv", function(error, temp) {
+    if (error) return console.error(error);
+    console.log(temp);
+    temp.forEach(function(d) {
+        d3.keys(d).forEach(function(k) {
+            if (k == "Percent") {
+                d[k] = +d[k]
+            }
+        })
+    })
+    byrace
+        .data(temp)
+        .type("bar")
+        .id("Year")
+        .x({ 'value': 'Period', 'label': { 'font': { 'size': 16 } } })
+        .y("Percent")
+        .time({ "value": "Year", "solo": 2016 })
+        .font({
+            "size": 14,
+            "family": "Raleway, sans-serif"
+        })
+        .title("Percent of All Graduates Employed in Ohio")
+        .title({
+            "sub": "Within six months and within one year after graduation.",
+            "total": false
+        })
+        .title({
+            "font": {
+                "family": "Montserrat, sans-serif",
+                "size": 16,
+                "transform": "uppercase"
+            }
+        })
+        .tooltip({ "font": { "family": "Raleway, sans-serif" } })
+        .tooltip(['value', 'Year'])
+        .tooltip({ "sub": "Race" })
+        .order({ "value": "Period" })
+        .text("Undergraduate")
+        .draw()
+
+
+})
