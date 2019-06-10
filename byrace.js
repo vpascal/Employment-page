ec0732c2ebd07788bd6d879ae6931d83ecb46e70
vpascal <vpascal2@gmail.com>
2019-06-10 15:19:32 -0400

    ready

--- byrace.js
+++ byrace.js
@@ -1,7 +1,6 @@
 var byrace = d3plus.viz().container("#byrace");
 d3.csv("data/byrace.csv", function(error, temp) {
     if (error) return console.error(error);
-    console.log(temp);
     temp.forEach(function(d) {
         d3.keys(d).forEach(function(k) {
             if (k == "Percent") {
@@ -12,15 +11,17 @@ d3.csv("data/byrace.csv", function(error, temp) {
     byrace
         .data(temp)
         .type("bar")
-        .id("Year")
+        .id("Race")
         .x({ 'value': 'Period', 'label': { 'font': { 'size': 16 } } })
-        .y("Percent")
-        .time({ "value": "Year", "solo": 2016 })
+        .y({ 'value': "Percent", 'range': [0, 100] })
+        .time({ "value": "Year", "solo": 2016, 'fixed': true })
+        .color('Race')
+        .text('Race')
         .font({
             "size": 14,
             "family": "Raleway, sans-serif"
         })
-        .title("Percent of All Graduates Employed in Ohio")
+        .title("Percent of All Graduates Employed in Ohio: By Race")
         .title({
             "sub": "Within six months and within one year after graduation.",
             "total": false
@@ -34,7 +35,6 @@ d3.csv("data/byrace.csv", function(error, temp) {
         })
         .tooltip({ "font": { "family": "Raleway, sans-serif" } })
         .tooltip(['value', 'Year'])
-        .tooltip({ "sub": "Race" })
         .order({ "value": "Period" })
         .text("Undergraduate")
         .draw()
